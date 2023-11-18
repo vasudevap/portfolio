d2fdaacc317c3378bec821919de282916dad4e82
PV <140992905+vasudevap@users.noreply.github.com>
2023-11-17 21:15:44 -0500

    UPDATED styling... working

--- Pages/Landing.jsx
+++ Pages/Landing.jsx
@@ -3,6 +3,10 @@ import Heading from "../src/components/Heading";
 import "./landing.css";
 
 function Landing() {
+  function handleContactMe(e) {
+    console.log(e.target);
+  }
+
   return (
     <div className="landingContainer">
       <div className="inlineTextContainer">
@@ -10,11 +14,15 @@ function Landing() {
         <div className="inlineTextDecorated inlineTextDecoratedBg">Welcome</div>
         <div>to my site.</div>
       </div>
-      <div className="contentContainerAvatar"></div>
+      <div className="avatar"></div>
+      <button className="contactMeBtn" onClick={handleContactMe}>
+        Contact Me
+      </button>
       <div className="contentContainer">
-        <div className="contentContainerLeft">
+        <div className="contentContainerBottom">
           <div>I am</div>
           <Heading content={{ text: "PRASHANT" }} />
+          <div>a full stack web app developer.</div>
         </div>
         <div></div>
       </div>
